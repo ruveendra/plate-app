ba7dff4f45b8f1110c9747471d242655bebfdb04
Ruveendra Dewshan <74244812+ruveendra@users.noreply.github.com>
2022-09-30 09:45:22 +0530

    Change Handler
    listen to editor changes so we can save the value somewhere.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,124 @@
-import React from 'react';
+// import React from 'react';
+// import { Plate } from '@udecode/plate';
+// import { editableProps } from './editableProps';
+// import { MyParagraphElement, MyH1Element,MyValue } from './plateTypes';
+
+// const initialValue = [
+//   {
+//     type: 'h1',
+//     children: [
+//       {
+//         text:
+//           'This is editable plain text with react and history plugins, just like a <textarea>!',
+//       },
+//     ],
+//   } as MyH1Element,
+// ];
+
+// export default () => (
+//   <Plate
+//   <MyValue> 
+//   editableProps={editableProps} 
+//   initialValue={initialValue} />
+// );
+
+//------------- Handlers ---------------
+
+import React, { useState } from 'react';
 import { Plate } from '@udecode/plate';
+import { plainTextValue } from './plainTextValue';
 import { editableProps } from './editableProps';
-import { MyParagraphElement, MyValue } from './plateTypes';
+import { MyValue } from './plateTypes';
 
-const initialValue = [
-  {
-    type: 'p',
-    children: [
-      {
-        text:
-          'This is editable plain text with react and history plugins, just like a <textarea>!',
-      },
-    ],
-  } as MyParagraphElement,
-];
+export default () => {
+  const [debugValue, setDebugValue] = useState<MyValue | null>(null);
 
-export default () => (
-  <Plate
-  <MyValue> 
-  editableProps={editableProps} 
-  initialValue={initialValue} />
-);
+  return (
+    <Plate<MyValue>
+      editableProps={editableProps}
+      initialValue={plainTextValue}
+      onChange={(newValue) => {
+        setDebugValue(newValue);
+        // save newValue...
+      }}
+    >
+      value: {JSON.stringify(debugValue)} //why convert to json 
+    </Plate>
+  );
+};
+
+
+
+///////// ---typescript---------
+
+
+// import React, { useState } from 'react';
+// import { Plate } from '@udecode/plate';
+// import { MyEditor, MyValue } from './plateTypes';
+
+// export default () => {
+//   // eslint-disable-next-line @typescript-eslint/no-use-before-define
+//   const [value, setValue] = useState(initialValue);
+
+//   return (
+//     // Because of the TypeScript-awareness you'll also get an error if you
+//     // initialize the editor with an invalid value or other invalid props.
+//     <Plate<MyValue, MyEditor> value={value} onChange={(v) => setValue(v)} />
+//   );
+// };
+
+// // Slate is TypeScript-aware, so if you try to use any unrecognizes `type`
+// // properties in this initial value you will get a compiler error.
+// const initialValue: MyValue = [
+//   {
+//     type: 'p',
+//     children: [
+//       {
+//         text: 'All the Slate examples are written in TypeScript. However, ',
+//       },
+//       { text: 'most', italic: true },
+//       { text: ' of them use ' },
+//       { text: 'implicit', bold: true },
+//       {
+//         text:
+//           " typings in many places because it makes it easier to see the actual Slate-specific code—especially for people who don't know TypeScript.",
+//       },
+//     ],
+//   },
+//   {
+//     type: 'p',
+//     children: [
+//       { text: 'This example is written with ' },
+//       { text: 'explicit', bold: true },
+//       {
+//         text:
+//           ' typings in all places, so you can see what a more realistic TypeScript usage would look like.',
+//       },
+//     ],
+//   },
+//   {
+//     type: 'h1',
+//     children: [{ text: 'Quotes' }],
+//   },
+//   {
+//     type: 'p',
+//     children: [{ text: "We'll throw in a few things like quotes…" }],
+//   },
+//   {
+//     type: 'blockquote',
+//     children: [{ text: 'A wise quote.' }],
+//   },
+//   {
+//     type: 'h1',
+//     children: [{ text: 'Images' }],
+//   },
+//   {
+//     type: 'p',
+//     children: [{ text: 'And images…' }],
+//   },
+//   {
+//     type: 'img',
+//     url: 'https://source.unsplash.com/kFrdX5IeQzI',
+//     children: [{ text: '' }],
+//   },
+// ];
